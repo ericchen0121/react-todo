309a10b58efd138ffa64e0caba924a02c61ea8a4
Eric Chen <ericchen0121@Erics-MacBook-Pro.local>
2017-03-12 19:03:00 -0700

    fix reducers

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -49,6 +49,11 @@ export var todosReducer = (state = [], action) => {
         }
       });
 
+    case 'ADD_TODOS':
+      return [
+        ...state,
+        ...action.todos
+      ]
     default:
       return state;
   }
